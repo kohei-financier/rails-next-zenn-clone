572647dd38d9c6e46b9264928271d836539e4e79
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-06-29 23:00:38 +0900

    add: Railsとの連携設定

--- next/src/styles/createEmotionCache.ts
+++ next/src/styles/createEmotionCache.ts
@@ -1,4 +1,4 @@
-import createCache, { EmotionCache } from "@emotion/cache"
+import createCache, { EmotionCache } from '@emotion/cache'
 
 export default function createEmotionCache(): EmotionCache {
   return createCache({ key: 'css' })
