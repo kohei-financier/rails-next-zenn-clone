a58130f7cb1a874dfd71d4e7d9a8719385210a02
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-11-02 17:00:36 +0900

    articleのindex実装

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::ArticlesController < ApplicationController
+  def index
+    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10)
+    render json: articles
+  end
+
   def show
     article = Article.published.find(params[:id])
     render json: article
