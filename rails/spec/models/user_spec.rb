6ca2c9e0cc6847f5ba0d8fbb05f06fb38a55e210
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-11-02 12:05:48 +0900

    add: Rspecのgemを追加・testの通過確認

---
+++ rails/spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  context "factoryのデフォルト設定に従った場合" do
+    let(:user) { create(:user) }
+
+    it "認証済みのuserレコードを正常に新規作成できる" do
+      expect(user).to be_valid
+      expect(user).to be_confirmed
+    end
+  end
+end
