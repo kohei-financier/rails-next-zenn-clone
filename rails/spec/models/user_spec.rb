9f05e8ab8780a1c75006e9f9c5255e88707d6774
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-11-02 17:45:14 +0900

    add: rspecテスト追加

--- rails/spec/models/user_spec.rb
+++ rails/spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   context "factoryのデフォルト設定に従った場合" do
     let(:user) { create(:user) }
 
--- rails/spec/requests/api/v1/articles_spec.rb
+++ rails/spec/requests/api/v1/articles_spec.rb
@@ -1,7 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Articles", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET api/v1/articles" do
+    subject { get(api_v1_articles_path(params)) }
+
+    before do
+      create_list(:article, 25, status: :published)
+      create_list(:article, 8, status: :draft)
+    end
+
+    context "pageをparamsで送信しない時" do
+      let(:params) { nil }
+
+      it "1ページ目のレコード10件取得" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.keys).to eq ["articles", "meta"]
+        expect(res["articles"].length).to eq 10
+        expect(res["articles"][0].keys).to eq ["id", "title", "content", "created_at", "from_today", "user"]
+        expect(res["articles"][0]["user"].keys).to eq ["name"]
+        expect(res["meta"].keys).to eq ["current_page", "total_pages"]
+        expect(res["meta"]["current_page"]).to eq 1
+        expect(res["meta"]["total_pages"]).to eq 3
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "pageをparamsで送信した時" do
+      let(:params) { { page: 2 } }
+
+      it "該当ページ目のレコード10件取得できる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.keys).to eq ["articles", "meta"]
+        expect(res["articles"].length).to eq 10
+        expect(res["articles"][0].keys).to eq ["id", "title", "content", "created_at", "from_today", "user"]
+        expect(res["articles"][0]["user"].keys).to eq ["name"]
+        expect(res["meta"].keys).to eq ["current_page", "total_pages"]
+        expect(res["meta"]["current_page"]).to eq 2
+        expect(res["meta"]["total_pages"]).to eq 3
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "GET api/v1/articles/:id" do
+    subject { get(api_v1_article_path(article_id)) }
+
+    let(:article) { create(:article, status:) }
+
+    context "article_idに対応するarticlesレコードが存在する時" do
+      let(:article_id) { article.id }
+
+      context "articlesレコードのステータスが公開中の時" do
+        let(:status) { :published }
+
+        it "正常にレコードを取得できる" do
+          subject
+          res = JSON.parse(response.body)
+          expect(res.keys).to eq ["id", "title", "content", "created_at", "from_today", "user"]
+          expect(res["user"].keys).to eq ["name"]
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context "articlesレコードのステータスが下書きの時" do
+        let(:status) { :draft }
+
+        it "404が返る" do
+          subject
+          expect(response).to have_http_status(404)
+        end
+      end
+    end
+
+    context "article_id に対応する articles レコードが存在しない時" do
+      let(:article_id) { 10_000_000_000 }
+
+      it "404が返る" do
+        subject
+        expect(response).to have_http_status(404)
+      end
+    end
   end
 end
